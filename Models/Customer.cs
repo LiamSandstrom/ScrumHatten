e56a94793d45fbec1a8d03af17067691cc3cc822
LiamSandstrom <113420205+LiamSandstrom@users.noreply.github.com>
2026-04-09 15:10:57 +0200

    Customer model

--- Models/Customer.cs
+++ Models/Customer.cs
@@ -1,7 +1,12 @@
+using MongoDB.Bson;
+using MongoDB.Bson.Serialization.Attributes;
+
 public class Customer
 {
     [BsonId]
     [BsonRepresentation(BsonType.ObjectId)]
-    public string Id { get; set; }
+    public string Phonenumber { get; set; }
     public string Name { get; set; }
+    public string Email { get; set; }
+    public string Adress { get; set; }
 }
