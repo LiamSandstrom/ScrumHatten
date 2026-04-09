8e2265713d91e2b058cf56d01e2bd1ddf388a70e
joeloman1 <berglund03@gmail.com>
2026-04-09 15:25:09 +0200

    CustomHat backend

--- Models/CustomHat.cs
+++ Models/CustomHat.cs
@@ -9,5 +9,8 @@ public class CustomHat
 {
     [BsonId]
     [BsonRepresentation(BsonType.ObjectId)]
-
+    public string Id { get; set; }
+    public string Name { get; set; }
+    public int Price { get; set; }
+    public List<HatMaterial> Materials { get; set; } = new();
 }
