bfda60d4939b8bc68752ebfecbc2e93286555fdc
LiamSandstrom <113420205+LiamSandstrom@users.noreply.github.com>
2026-04-09 15:19:46 +0200

    add material model

---
+++ Models/Material.cs
@@ -0,0 +1,12 @@
+using MongoDB.Bson;
+using MongoDB.Bson.Serialization.Attributes;
+
+public class Material
+{
+    [BsonId]
+    [BsonRepresentation(BsonType.ObjectId)]
+    public string Id { get; set; }
+    public string Name { get; set; }
+    public int Quantity { get; set; }
+    public string Price { get; set; }
+}
