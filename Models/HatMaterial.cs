3528d80dcb78873486cbb6fe1c3b6bbe89a92d7b
joeloman1 <berglund03@gmail.com>
2026-04-09 14:58:01 +0200

    Backend

---
+++ Models/HatMaterial.cs
@@ -0,0 +1,16 @@
+﻿using MongoDB.Bson;
+using MongoDB.Bson.Serialization.Attributes;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Models
+{
+    public class HatMaterial
+    {
+        [BsonId]
+        [BsonRepresentation(BsonType.ObjectId)]
+        public string MaterialId { get; set; }
+        public int Amount { get; set; }
+    }
+}
