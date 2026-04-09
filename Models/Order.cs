73fe572acbd4500dbfb9f21da8d40fe822977af1
hilleviengelhart <hilleviengelhart@gmail.com>
2026-04-09 15:22:59 +0200

    hej

--- Models/Order.cs
+++ Models/Order.cs
@@ -1,10 +1,34 @@
-﻿using System;
+﻿using MongoDB.Bson;
+using MongoDB.Bson.Serialization.Attributes;
+using System;
 using System.Collections.Generic;
 using System.Text;
 
 namespace Models
 {
-    internal class Order
+    public class Order
     {
+        [BsonId]
+        [BsonRepresentation(BsonType.ObjectId)]
+        public string Id { get; set; }
+
+        public List<HatMaterial> Materials { get; set; } = new();
+
+        public List<Hat> Hats { get; set; } = new();
+
+        public decimal TimeToMake { get; set; } 
+
+        public string DateToFinish { get; set; }
+
+        public int HatAmount { get; set; }  
+
+        public decimal TransportPrice { get; set; } 
+
+        public decimal Moms {  get; set; }
+
+        public Boolean FastOrder { get; set; }  
+
+        
+
     }
 }
