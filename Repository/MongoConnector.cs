9e92b8f1107e5b5213d517f180c479d1b151331d
LiamSandstrom <113420205+LiamSandstrom@users.noreply.github.com>
2026-04-09 12:08:02 +0200

    fix mongo db

---
+++ Repository/MongoConnector.cs
@@ -0,0 +1,25 @@
+using MongoDB.Driver;
+using MongoDB.Bson;
+namespace Repository
+{
+    public class MongoConnector
+    {
+        private readonly IMongoDatabase _database;
+
+        public MongoConnector(string connectionString)
+        {
+            var client = new MongoClient(connectionString);
+            _database = client.GetDatabase("ScrumHatten");
+            try
+            {
+                var result = _database.RunCommand<BsonDocument>(new BsonDocument("ping", 1));
+                Console.WriteLine("Ansluten till MongoDB!");
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine($"Anslutning misslyckades: {ex.Message}");
+            }
+        }
+
+    }
+}
