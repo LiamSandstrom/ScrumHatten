c5f18df7a5437187a2183eebeabe14ee8676944a
agngara <agngara@gmail.com>
2026-04-09 15:24:27 +0200

    Klassen MaterialOrder klar
    Jättebra!

---
+++ Models/MaterialOder.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using MongoDB.Bson;
+using MongoDB.Bson.Serialization.Attributes;
+
+namespace Models
+{
+    public class MaterialOder
+    {
+
+
+        [BsonId]
+        [BsonRepresentation(BsonType.ObjectId)]
+
+        [BsonElement("Amound")]
+        public string Amount {  get; set; }
+
+
+        [BsonElement("Price")]
+        public decimal Price { get; set; }
+      
+     
+        // Lägg till Material
+
+       // Lägg in anställd
+
+
+        public MaterialOder(string amound,  decimal price,  HatMaterial hatMaterial)
+        {
+            Amount = amound;
+            Price = price;
+            HatMaterial = hatMaterial;
+        }
+
+       
+    }
+}
