598b4cc6780824a0e82b174543d2a4202776dd82
LiamSandstrom <113420205+LiamSandstrom@users.noreply.github.com>
2026-04-10 11:01:08 +0200

    user and user roles

--- Models/User.cs
+++ Models/User.cs
@@ -3,18 +3,10 @@ using MongoDbGenericRepository.Attributes;
 
 namespace Models
 {
-    public enum UserRole
-    {
-        Admin,
-        User
-    }
-
     public class User : MongoIdentityUser<Guid>
     {
         public string Name { get; set; }
         public bool IsAdmin { get; set; }
-        public UserRole Role { get; set; }
     }
-
 }
 
