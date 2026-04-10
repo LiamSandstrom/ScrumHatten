9bf6ab0a049c689bf97a8b93c353bfc662291ceb
LiamSandstrom <113420205+LiamSandstrom@users.noreply.github.com>
2026-04-10 11:01:08 +0200

    user and user roles

--- MVC/Program.cs
+++ MVC/Program.cs
@@ -1,32 +1,76 @@
 using Repository;
+using Microsoft.AspNetCore.Identity;
+using AspNetCore.Identity.MongoDbCore.Infrastructure;
+using AspNetCore.Identity.MongoDbCore.Extensions;
+using Models;
+
 var builder = WebApplication.CreateBuilder(args);
 
-// Add services to the container.
 builder.Services.AddControllersWithViews();
+
 var connectionString = builder.Configuration["MongoDB:ConnectionString"];
 builder.Services.AddSingleton<MongoConnector>(new MongoConnector(connectionString));
 
+builder.Services.AddIdentity<User, ApplicationRole>(options =>
+{
+    options.Password.RequiredLength = 8;
+    options.Password.RequireDigit = false;
+    options.Password.RequireUppercase = false;
+    options.Password.RequireNonAlphanumeric = false;
+    options.User.RequireUniqueEmail = false;
+    options.Lockout.MaxFailedAccessAttempts = 5;
+    options.Lockout.DefaultLockoutTimeSpan = TimeSpan.FromMinutes(5);
+})
+.AddMongoDbStores<User, ApplicationRole, Guid>(
+    builder.Configuration["MongoDB:ConnectionString"],
+    builder.Configuration["ScrumHatten"]
+)
+.AddDefaultTokenProviders();
+
+// Cookie-inställningar
+builder.Services.ConfigureApplicationCookie(options =>
+{
+    options.LoginPath = "/Account/Login";
+    options.LogoutPath = "/Account/Logout";
+    options.AccessDeniedPath = "/Account/AccessDenied";
+});
+
 var app = builder.Build();
 
-// Configure the HTTP request pipeline.
+//Create Roles 
+using (var scope = app.Services.CreateScope())
+{
+    var roleManager = scope.ServiceProvider
+        .GetRequiredService<RoleManager<ApplicationRole>>();
+
+    string[] roller = { "Admin", "User" };
+
+    foreach (var roll in roller)
+    {
+        if (!await roleManager.RoleExistsAsync(roll))
+        {
+            await roleManager.CreateAsync(new ApplicationRole { Name = roll });
+        }
+    }
+}
+
 if (!app.Environment.IsDevelopment())
 {
     app.UseExceptionHandler("/Home/Error");
-    // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
     app.UseHsts();
 }
 
 app.UseHttpsRedirection();
+app.UseStaticFiles();
 app.UseRouting();
 
+app.UseAuthentication();
 app.UseAuthorization();
 
 app.MapStaticAssets();
-
 app.MapControllerRoute(
     name: "default",
     pattern: "{controller=Home}/{action=Index}/{id?}")
     .WithStaticAssets();
-
 
 app.Run();
