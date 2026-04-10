c6226992fe84493000fb2dc029addb138e1ea2fc
joeloman1 <berglund03@gmail.com>
2026-04-10 10:36:13 +0200

    Backend-krux

---
+++ Repository/IHatInterface.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Repository
+{
+    internal interface IHatInterface
+    {
+        Task<List<String>> GetAllHats();
+        Task<List<String>> GetStockedHats();
+        Task<List<String>> GetCustomHats();
+        Task<List<int>> GetQuantityById(string id);
+        Task<List<double>> GetPriceById(string id);
+        
+    }
+}
