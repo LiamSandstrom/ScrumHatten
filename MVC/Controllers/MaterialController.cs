d237c69e195cafd123b2f34765c237c557d09b24
joeloman1 <berglund03@gmail.com>
2026-04-10 14:17:27 +0200

    fortsättning material

--- MVC/Controllers/MaterialController.cs
+++ MVC/Controllers/MaterialController.cs
@@ -6,15 +6,45 @@ using Repository;
 
 namespace MVC.Controllers { 
 
-    public class MaterialController : ControllerBase
+    public class MaterialController : Controller
     {
+        public IActionResult Material()
+        {
+            return View();
+        }
         private readonly IMaterialRepository _materialRepository;
 
         [HttpGet("names")]
         public async Task<IActionResult> GetNames()
         {
-            var names 
+            var names = await _materialRepository.GetAllMaterialsAsync();
+            return Ok(names);
+        }
+
+        [HttpGet("unit")]
+        public async Task<IActionResult> GetUnitById(string id)
+        {
+            var unit = await _materialRepository.GetUnitByIdAsync(id);
+            return Ok(unit);
+        }
+
+        [HttpGet("quantity")]
+        public async Task<IActionResult> GetQuantityById(string id)
+        {
+            var quantity = await _materialRepository.GetQuantityByIdAsync(id);
+            return Ok(quantity);
+        }
+
+        [HttpGet("price")] 
+        public async Task<IActionResult> GetPriceById(string id)
+            {
+            var price = await _materialRepository.GetPriceByIdAsync(id);
+            return Ok(price);
         }
 
     }
 }
+
+//Task<String> GetUnitByIdAsync(string id);
+//Task<int> GetQuantityByIdAsync(string id);
+//Task<double> GetPriceByIdAsync(string id);
