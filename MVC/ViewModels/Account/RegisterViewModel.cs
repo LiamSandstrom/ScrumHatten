49fe360b8162d19828bba9b4c3803f133f8a5962
01010100heo <wikstromtheo@gmail.com>
2026-04-13 14:08:12 +0200

    Registrering

--- MVC/ViewModels/Account/RegisterViewModel.cs
+++ MVC/ViewModels/Account/RegisterViewModel.cs
@@ -8,33 +8,39 @@ namespace MVC.Models.Account
         [Required(ErrorMessage = "Please Enter a Name")]
         [StringLength(30)]
         [RegularExpression(@"^[a-zA-Z0-9]+$", ErrorMessage = "Name can only contain letters and numbers")]
-        public required string Name { get; set; }
+        public string Name { get; set; }
 
 
         [Required(ErrorMessage = "Please Enter an Email")]
         [EmailAddress(ErrorMessage = "Please enter a valid email address")]
         [StringLength(100)]
-        public required string Email { get; set; }
+        public string Email { get; set; }
 
 
 
 
         [Required(ErrorMessage ="Please enter an phonenumer")]
         [Phone(ErrorMessage ="Please eneter a valid pohonenumer")]
-        public required string Phonenumber { get; set; }
+        public string Phonenumber { get; set; }
 
 
 
 
         [Required(ErrorMessage = "Please Enter a Password")]
         [DataType(DataType.Password)]
-        public required string Password { get; set; }
+        public string Password { get; set; }
 
         [Required(ErrorMessage = "Please confirm Password")]
         [DataType(DataType.Password)]
         [Display(Name = "Confirm password")]
         [Compare("Password")]
-        public required string ConfirmPassword { get; set; }
+        public string ConfirmPassword { get; set; }
+
+
+        public List<string> roles { get; set; } = new();
+
+        [Required(ErrorMessage = "Var god välj en roll")]
+        public string selectedRole { get; set; }
 
 
     }
