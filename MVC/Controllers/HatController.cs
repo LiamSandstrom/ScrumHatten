3f69972f9a04f4bb2d7afe1cfda7233edcddc98d
carolinehultman <caroline.hultman25@gmail.com>
2026-04-13 10:52:25 +0200

    returnerar tom lista

--- MVC/Controllers/HatController.cs
+++ MVC/Controllers/HatController.cs
@@ -13,40 +13,40 @@ namespace MVC.Controllers
             _hatService = hatService;
         }
 
-       // // HAR SVÅRT ATT KOPPLA MOT DATABASEN
-       // public IActionResult Index()
-       //{
-       //     List<Hat> hats = _hatService.GetAllHats();
-       //     return View(hats);
-       // }
-    
-
-    public IActionResult Index()
+        // // HAR SVÅRT ATT KOPPLA MOT DATABASEN
+        public IActionResult Index()
         {
-            List<Hat> hats = new List<Hat>
-            {
-                new Hat
-                {
-                    Id = "1",
-                    Name = "Sommarhatt",
-                    Description = "En ljus och fin sommarhatt.",
-                    ImageUrl = "https://via.placeholder.com/300x250",
-                    Price = 299,
-                    Quantity = 5
-                },
-                new Hat
-                {
-                    Id = "2",
-                    Name = "Vinterhatt",
-                    Description = "En varm hatt för kalla dagar.",
-                    ImageUrl = "https://via.placeholder.com/300x250",
-                    Price = 399,
-                    Quantity = 2
-                }
-            };
-
+            List<Hat> hats = _hatService.GetAllHats() ?? new List<Hat>();
             return View(hats);
-
         }
+
+        //TESATDATA
+        //public IActionResult Index()
+        //{
+        //    List<Hat> hats = new List<Hat>
+        //    {
+        //        new Hat
+        //        {
+        //            Id = "1",
+        //            Name = "Sommarhatt",
+        //            Description = "En ljus och fin sommarhatt.",
+        //            ImageUrl = "https://via.placeholder.com/300x250",
+        //            Price = 299,
+        //            Quantity = 5
+        //        },
+        //        new Hat
+        //        {
+        //            Id = "2",
+        //            Name = "Vinterhatt",
+        //            Description = "En varm hatt för kalla dagar.",
+        //            ImageUrl = "https://via.placeholder.com/300x250",
+        //            Price = 399,
+        //            Quantity = 2
+        //        }
+        //    };
+
+        //    return View(hats);
+
     }
-}
+    }
+
