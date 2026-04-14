b0166f88a93779acbd6147cc92238e9fb3d6f8a1
carolinehultman <caroline.hultman25@gmail.com>
2026-04-14 10:05:07 +0200

    g

--- Models/Hat.cs
+++ Models/Hat.cs
@@ -4,12 +4,20 @@ using System.Collections.Generic;
 
 namespace Models
 {
+    public class Hat
     {
         [BsonId]
         [BsonRepresentation(BsonType.ObjectId)]
         public string Id { get; set; }
         public string Name { get; set; }
+<<<<<<< HEAD
         public double Price { get; set; }
+=======
+        public string Description { get; set; }
+        public string ImageUrl { get; set; }
+        public int Price { get; set; }
+>>>>>>> C_E
         public List<HatMaterial> Materials { get; set; } = new();
         public int Quantity { get; set; }
     }
+}
