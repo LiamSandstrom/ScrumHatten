b0dfcc72aa0d843df8ab2fab8e4cf02698c056ed
elinmariaaa <elinsfotboll97@hotmail.com>
2026-04-15 10:22:26 +0200

    h

---
+++ MVC/wwwroot/js/material 2.js
@@ -0,0 +1,58 @@
+function saveMaterial() {
+    const materialData = {
+        Name: document.getElementById('matName').value,
+        Unit: document.getElementById('matUnit').value,
+        PricePerUnit: parseFloat(document.getElementById('matPrice').value),
+        Quantity: parseFloat(document.getElementById('matAmount').value) || 0
+    };
+
+    fetch('/Material/Create', {
+        method: 'POST',
+        headers: {
+            'Content-Type': 'application/json'
+        },
+        body: JSON.stringify(materialData)
+    })
+    .then(response => {
+        if (response.ok) {
+            alert('Materialet har sparats!');
+            location.reload();
+        } else {
+            alert("Gick inte att spara materialet.");
+        }
+
+    })
+    .catch(error => console.error('Error:', error));
+
+}
+
+function updateStock(id) {
+const inputField = document.getElementById(`restock-${id}`);
+const addedAmount = parseFloat(inputField.value);
+
+if (isNaN(addedAmount) || addedAmount <= 0 ) {
+
+    alert("Vänligen ange en giltig mängd.");
+    return;
+}
+fetch(`/Material/Restock/${id}`,{
+
+    method: 'PATCH',
+    headers: { 'Content-Type': 'application/json'},
+    body: JSON.stringify(addedAmount)
+})
+.then(response => {
+
+    if (response.ok) {
+
+        location.reload(); 
+
+    }else{
+
+        alert("Kunde inte uppdatera lagret.");
+
+    }
+
+    });
+
+}
