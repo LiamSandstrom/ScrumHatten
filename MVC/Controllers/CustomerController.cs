466e757cfe1247135db0a8ebe7f7dabeb37240b8
agngara <agngara@gmail.com>
2026-04-16 16:14:43 +0200

    kundhantering

--- MVC/Controllers/CustomerController.cs
+++ MVC/Controllers/CustomerController.cs
@@ -1,4 +1,5 @@
 using Microsoft.AspNetCore.Mvc;
+using MVC.ViewModels.CustomerViewModels;
 using Repository;
 
 namespace MVC.Controllers
@@ -13,11 +14,80 @@ namespace MVC.Controllers
             _customerRepository = customerRepository;
         }
 
-        [HttpGet]
-        public async Task<IActionResult> Index()
+
+
+
+
+
+        [HttpGet("CustomerList")]
+        public async Task<IActionResult> CustomerList()
         {
-            var customers = await _customerRepository.GetAllCustomersAsync();
-            return View(customers);
+            List<Customer> customerList = new List<Customer>();
+            customerList = await _customerRepository.GetAllCustomersAsync();
+
+
+            CustomerListViewModel vm = new CustomerListViewModel()
+            {
+                allCustomers = customerList
+            };
+
+            return View(vm);
+        }
+
+
+
+
+        //[HttpGet]
+        //public async Task<IActionResult> Index()
+        //{
+        //    var customers = await _customerRepository.GetAllCustomersAsync();
+        //    return View(customers);
+        //}
+
+        [HttpGet("Create")]
+        public IActionResult Create()
+        {
+            CreateCustomerViewModel vm = new CreateCustomerViewModel();
+
+            return View(vm);
+        }
+
+        [HttpGet("Edit")]
+        public async Task<IActionResult> Edit(string id)
+        {
+            Customer customer = await _customerRepository.GetCustomerByIdAsync(id);
+
+            EditCustomerViewModel vm = new EditCustomerViewModel
+            {
+                CustomerId = customer.Id,
+                Name = customer.Name,
+                Email = customer.Email,
+                PhoneNumber = customer.PhoneNumber,
+                Adress = customer.Adress,
+                ZipCode = customer.ZipCode,
+                Country = customer.Country,
+            };
+
+            return View(vm);
+        }
+
+        [HttpPost("Edit")]
+        public async Task<IActionResult> Edit(EditCustomerViewModel vm)
+        {
+            Customer customer = new Customer
+            {
+                Id = vm.CustomerId,
+                Name = vm.Name,
+                Email = vm.Email,
+                PhoneNumber = vm.PhoneNumber,
+                Adress = vm.Adress,
+                ZipCode = vm.ZipCode,
+                Country = vm.Country,
+            };
+
+            await Update(customer);
+
+            return RedirectToAction(nameof(CustomerList));
         }
 
         [HttpGet("{id}")]
@@ -38,6 +108,34 @@ namespace MVC.Controllers
             return Ok(customer);
         }
 
+
+
+        [HttpPost("CreateCustomer")]
+        public async Task<IActionResult> CreateCustomer(CreateCustomerViewModel vm)
+        {
+            if (!ModelState.IsValid)
+            {
+                return View("Index", await _customerRepository.GetAllCustomersAsync());
+            }
+
+            Customer customer = new Customer
+            {
+
+                Name = vm.Name,
+                PhoneNumber = vm.PhoneNumber,
+                Email = vm.Email,
+                Adress = vm.Adress,
+                ZipCode = vm.ZipCode,
+                Country = vm.Country
+
+            };
+
+            await _customerRepository.AddCustomerAsync(customer);
+            TempData["SuccessMessage"] = "Kunden har lagts till!";
+            return RedirectToAction(nameof(CustomerList));
+        }
+
+
         [HttpPost("Create")]
         public async Task<IActionResult> Create(Customer customer)
         {
@@ -48,7 +146,7 @@ namespace MVC.Controllers
 
             await _customerRepository.AddCustomerAsync(customer);
             TempData["SuccessMessage"] = "Kunden har lagts till!";
-            return RedirectToAction(nameof(Index));
+            return RedirectToAction(nameof(CustomerList));
         }
 
         [HttpPost("Update")]
@@ -62,7 +160,7 @@ namespace MVC.Controllers
             await _customerRepository.UpdateCustomerAsync(customer);
 
             TempData["SuccessMessage"] = "Kunduppgifter uppdaterade!";
-            return RedirectToAction(nameof(Index));
+            return RedirectToAction(nameof(CustomerList));
         }
 
         [HttpPost("Delete/{id}")]
@@ -75,7 +173,7 @@ namespace MVC.Controllers
 
             await _customerRepository.DeleteCustomerAsync(id);
             TempData["SuccessMessage"] = "Kunden har raderats.";
-            return RedirectToAction(nameof(Index));
+            return RedirectToAction(nameof(CustomerList));
         }
     }
 }
