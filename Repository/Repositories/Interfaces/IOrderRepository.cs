18a048d7280c3054d62ba649c292d2de214bdb09
svanteskold <svante.skolds@gmail.com>
2026-04-16 13:20:00 +0200

    fix med order
    .

--- Repository/Repositories/Interfaces/IOrderRepository.cs
+++ Repository/Repositories/Interfaces/IOrderRepository.cs
@@ -1,18 +1,24 @@
 using Models;
+using System;
+using System.Collections.Generic;
+using System.Threading.Tasks;
 
 namespace Repository
 {
     public interface IOrderRepository
     {
-        Task<String> GetOrderByIdAsync(string id);
+        Task<Order> GetOrderByIdAsync(string id);
+        Task<List<Order>> GetAllOrdersAsync();
         Task<List<Hat>> GetHatsByOrderIdAsync(string id);
         Task<List<HatMaterial>> GetMaterialsByOrderIdAsync(string id);
         Task<decimal> GetTimeToMakeByOrderIdAsync(string id);
         Task CreateOrderAsync(Order newOrder);
         Task UpdateOrderAsync(string id, Order updatedOrder);
         Task DeleteOrderAsync(string id);
-        Task GetOrderByStatusAsync(string status);
-        Task GetOrderByMakerIdAsync(Guid makerId);
-        Task <decimal> CalculateTotalPriceByOrderIdAsync(string id);
+        Task<List<Order>> GetOrderByStatusAsync(string status);
+        Task<List<Order>> GetOrderByMakerIdAsync(Guid makerId);
+
+        Task UpdateOrderStatusAsync(string id, string newStatus); 
+
     }
 }
