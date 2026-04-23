be90965d9ec9bd87bac5232c12c78cf4b82b192b
elinmariaaa <elinsfotboll97@hotmail.com>
2026-04-23 15:04:36 +0200

    h

--- Models/HatSize.cs
+++ Models/HatSize.cs
@@ -1,5 +1,8 @@
+using MongoDB.Bson.Serialization.Attributes;
+
 namespace Models
 {
+    [BsonIgnoreExtraElements]
     public class HatSize
     {
         public string Label { get; set; }      // t.ex. XS(50-52cm), S, M
