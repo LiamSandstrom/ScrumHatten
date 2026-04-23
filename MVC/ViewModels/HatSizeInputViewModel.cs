9669cf8f0f42b0a450069d8a10594596a81e6537
elinmariaaa <elinsfotboll97@hotmail.com>
2026-04-23 14:43:49 +0200

    strl

--- MVC/ViewModels/HatSizeInputViewModel.cs
+++ MVC/ViewModels/HatSizeInputViewModel.cs
@@ -3,6 +3,6 @@ namespace MVC.ViewModels
     public class HatSizeInputViewModel
     {
         public string Label { get; set; }   // t.ex. XS, S, M
-        public int Centimeters { get; set; } // t.ex. 52, 54, 56
+        public int Quantity { get; set; } 
     }
 }
